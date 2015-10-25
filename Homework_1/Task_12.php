6948fc2228d40f5a9e99a122763127f30a9fac5b
Just Man <g.staikov85@gmail.com>
2015-10-25 08:52:38 +0200

    Homework_1

---
+++ Homework_1/Task_12.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: just
+ * Date: 25.10.15
+ * Time: 07:27
+ */
+
+/*
+    * Задача 12:
+    * Високосни години са всички години кратни на 4 с изключения
+    * столетията, но без столетия кратни на 400, т.е. 1900 не е високосна,
+    * но 1600 и 2000 са високосни.
+    * Съставете програма, която по дадени ден, месец, година отпечатва
+    * следващата дата.
+    * Входни данни: три числа за ден, месец, година.
+    * Пример: 28, 2, 2000
+    * Изход: 1,3,2000
+*/
+
+$day = readline('Enter desire day');
+$month = readline('Enter desire month');
+$year = readline('Enter desire year');
+switch ($month) {
+    case '1' : $monthDays = 31;
+        break;
+    case '2' : $monthDays = 28;
+        break;
+    case '3' : $monthDays = 31;
+        break;
+    case '4' : $monthDays = 30;
+        break;
+    case '5' : $monthDays = 31;
+        break;
+    case '6' : $monthDays = 30;
+        break;
+    case '7' : $monthDays = 31;
+        break;
+    case '8' : $monthDays = 31;
+        break;
+    case '9' : $monthDays = 30;
+        break;
+    case '10' : $monthDays = 31;
+        break;
+    case '11' : $monthDays = 30;
+        break;
+    case '12' : $monthDays = 31;
+        break;
+}
+if (!($year % 4) && !($year % 100) && !($year % 400) && $month == 2) {
+    echo 'This is leap year';
+    $monthDays = 29;
+}
+if ($day < $monthDays) {
+    $day +=1;
+} else if ($month < 12){
+    $day = 1;
+    $month +=1;
+} else {
+    $day = 1;
+    $month = 1;
+    $year += 1;
+}
+echo 'day.month.year'. PHP_EOL;
+echo $day . '.' . $month . '.'  . $year;
