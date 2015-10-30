0182e8a66354e3e81f30d2ddb2d55e3f04876fca
Just Man <g.staikov85@gmail.com>
2015-10-30 11:50:09 +0200

    Homework_2

---
+++ Lection3/Homework_2/Task_1.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: just
+ * Date: 30.10.15
+ * Time: 10:00
+ */
+
+//Task_1
+
+//Задача 1: Да се изведат на екрана числата от 1 до 100.
+
+for ($i = 0; $i < 100; $i++) {
+    echo $i , PHP_EOL;
+}
