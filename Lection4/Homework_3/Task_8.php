e2f52e0a992c0a5b6907358a241f6422f5fde285
Just Man <g.staikov85@gmail.com>
2015-11-05 22:24:09 +0200

    Homework_3

---
+++ Lection4/Homework_3/Task_8.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: just
+ * Date: 05.11.15
+ * Time: 21:26
+ */
+
+/*
+Задача 8:
+Напишете програма, която намира и извежда най-дългата редица от
+еднакви поредни елементи в даден масив.
+Пример:
+Въведете елементите на масива:
+2,1,1,2,3,3,2,2,2,1
+Максималната редица е: 2 2 2
+*/
+
+//Declaration variables
+$numbers = [];
+$temp = [];
+$final = [];
+
+
+//Enter value &n body
+$end = readline('Enter number of desire elements');
+for ($i = 0; $i < $end ; $i += 1) {
+    $number = readline('Enter element of array');
+    $numbers[] = $number;
+    if ($i >= 1) {
+        if ($numbers[$i] === $numbers[$i - 1]) {
+            $temp[] = $numbers[$i];
+        } else {
+            $tempLen = count($temp);
+            if ($tempLen > 0){
+                $temp[] = $temp[0];
+                $tempLen = count($temp);
+                $finalLen = count($final);
+                if ($finalLen < $tempLen) {
+                    $final = $temp;
+                    $temp = [];
+            }
+            }
+        }
+    }
+}
+print_r($temp);
+print_r($final);
