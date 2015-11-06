74d8b185fca4fa03ffcd54b6a0d12b0ed27a240d
Just Man <g.staikov85@gmail.com>
2015-11-06 09:28:43 +0200

    Добавяне на коментари относно работата на кода

--- Lection4/Homework_3/Task_8.php
+++ Lection4/Homework_3/Task_8.php
@@ -28,28 +28,32 @@ $finalLen = 0;
 $end = readline('Enter number of desire elements');
 for ($i = 0; $i < $end ; $i += 1) {
     $number = readline('Enter element of array');
+    //Body
     $numbers[] = $number;
-    if ($i >= 1) {
-        if ($numbers[$i] === $numbers[$i - 1]) {
-            $temp[] = $numbers[$i];
-        } else {
-            $tempLen = count($temp);
-            if ($tempLen > 0){
-                $finalLen = count($final);
-                if ($finalLen < $tempLen) {
-                    $final = $temp;
-                    $temp = [];
+    if ($i >= 1) {                                  //Правим проверка дали сме вече на втори елемент от масива
+        if ($numbers[$i] === $numbers[$i - 1]) {    //Проверяваме дали текущият елемент е равен на предния
+            $temp[] = $numbers[$i];                 //При равенство м/у двата елемента добавяме към временен масив
+        } else {                                    //Ако двата елемента не са равни прехвърляме временния и го зануляваме
+            $tempLen = count($temp);                //Проверяваме дължината на временния масив
+            if ($tempLen > 0){                      //ако тя е по-голяма от 0
+                $finalLen = count($final);          //проверяваме и големината на финалният масив
+                if ($finalLen < $tempLen) {         //ако е по-малка от тази на временния
+                    $final = $temp;                 //финалния става равен на временния,
+                    $temp = [];                     //след което зануляваме временния
                 } else {
-                    $temp = [];
+                    $temp = [];                     //Ако дължината на временния е по-малка само го зануляваме
                     }
             }
         }
     }
 }
-if ($finalLen == $tempLen) {
-    $temp[] = $temp[0];
-    $final[] = $final[0];
-    $tempLen = count($temp);
+
+//Print
+$tempLen = count($temp);
+$finalLen = count($final);
+if ($finalLen == $tempLen) {                        //Проверяваме дали дължината на временния и финалния са равни
+    $temp[] = $temp[0];                             //Добавяме един елемент заради това че при проверката винаги се изпуска 1
+    $final[] = $final[0];                           //Добавяме един елемент заради това че при проверката винаги се изпуска 1
     $finalLen = count($final);
     echo 'Array 1' . ' Array 2' ,PHP_EOL;
     for ($i = 0; $i < $finalLen ; $i += 1) {
