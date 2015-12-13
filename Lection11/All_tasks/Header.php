06eb5985eaa48363a04fae7fbb656ce4b8b26a7e
just <g.staikov85@gmail.com>
2015-12-13 09:22:36 +0200

    Homework_HTML&PHP

--- Lection11/All_tasks/Header.php
+++ Lection11/All_tasks/Header.php
@@ -15,7 +15,7 @@ $logged = $_SESSION['logged'];
                     <li>Staykov</li>
                     <li>5</li>
                     <li>Web 3</li>
-                    <li><a href="#">Link</a> </li>
+                    <li><a href="index.php">Link</a> </li>
                     <li>justaman_85</li>
                 </ul>
             </div>
@@ -33,12 +33,12 @@ $logged = $_SESSION['logged'];
         </header>
         <nav>
             <ul>
-                <li><a href="<?php if ($_SESSION['logged']){echo 'index.php';} else {echo '#';}?>">Home</a></li>
-                <li><a href="<?php if ($_SESSION['logged']){echo 'about_me.php';} else {echo '#';}?>">About me</a></li>
-                <li><a href="<?php if ($_SESSION['logged']){echo 'skills.php';} else {echo '#';}?>">Skills</a></li>
-                <li><a href="<?php if ($_SESSION['logged']){echo 'contact.php';} else {echo '#';}?>">Contacts</a></li>
-                <li><a href="<?php if ($_SESSION['logged']){echo 'gallery.php';} else {echo '#';}?>">Gallery</a></li>
-                <li><a href="<?php if ($_SESSION['logged']){echo 'color_game.php';} else {echo '#';}?>">Color game</a></li>
+                <li><a href="<?php if ($_SESSION['logged']){echo 'index.php';} else {echo 'index.php';}?>">Home</a></li>
+                <li><a href="<?php if ($_SESSION['logged']){echo 'about_me.php';} else {echo 'index.php';}?>">About me</a></li>
+                <li><a href="<?php if ($_SESSION['logged']){echo 'skills.php';} else {echo 'index.php';}?>">Skills</a></li>
+                <li><a href="<?php if ($_SESSION['logged']){echo 'contact.php';} else {echo 'index.php';}?>">Contacts</a></li>
+                <li><a href="<?php if ($_SESSION['logged']){echo 'gallery.php';} else {echo 'index.php';}?>">Gallery</a></li>
+                <li><a href="<?php if ($_SESSION['logged']){echo 'color_game.php';} else {echo 'index.php';}?>">Color game</a></li>
             </ul>
         </nav>
     <section>
--- Lection11/All_tasks/color_game.php
+++ Lection11/All_tasks/color_game.php
@@ -1,2 +1,33 @@
-<?php require_once "Header.php"; ?>
-<?php require_once "Footer.php";?>
+<?php require_once "Header.php";
+    $colors = ['#000000', '#808080', '#C0C0C0', '#FFFFFF',
+               '#800000', '#FF0000', '#808000', '#FFFF00',
+               '#008000', '#00FF00', '#008080', '#00FFff',
+               '#000080', '#0000FF', '#800080', '#FF00FF'];
+    $lastRand = null;
+
+    if (isset($_POST['ansform']) || empty($rand)) {
+    $answer = array_rand($colors, 4);
+    $answers = [];
+    for ($i = 0; $i < 4 ; $i += 1) {
+        $answers[$i] = $colors[$answer[$i]];
+    }
+    $lastRand = $_SESSION['rand'];
+    $rand = array_rand($answer, 1);
+    $_SESSION['rand'] = $rand;
+    $color = $answers[$rand];}
+?>
+    <div style="width: 200px;height: 200px; background: <?php echo $color ?> "></div>
+    <form action="" method="post" name="ansform">
+        <button name="ans" value="0"><?php echo $answers[0]?></button>
+        <button name="ans" value="1"><?php echo $answers[1]?></button>
+        <button name="ans" value="2"><?php echo $answers[2]?></button>
+        <button name="ans" value="3"><?php echo $answers[3]?></button>
+    </form>
+<?php
+$ans = $_POST['ans'];
+if ($ans == $lastRand) {?>
+    <p>Верен отговор</p>
+<?php } else {?>
+    <p>Грешен отговор</p>
+<?php }
+require_once "Footer.php";?>
