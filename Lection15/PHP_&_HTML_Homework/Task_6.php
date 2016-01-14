2a46a94f811ac98d241ad24e099d7da9c4ae007e
Just Man <g.staikov85@gmail.com>
2016-01-14 07:57:41 +0200

    Homework_6

--- Lection15/PHP_&_HTML_Homework/Task_6.php
+++ Lection15/PHP_&_HTML_Homework/Task_6.php
@@ -12,4 +12,25 @@
 година). Ако потребителя не е въвел някое поле, нека въведената от
 него стойност да се запази във формата.
 */
-
+require_once 'My_library.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet" type="text/css" href="reset.css">
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+<form action="" method="post">
+			<div class="<?= getFieldClass($validationErrors, 'firstname')?>">
+				<label for="firstname">First Name</label>
+				<input type="text" name="firstname" id="firstname" value="<?= htmlentities($firstName)?>"/>
+				<?= displayErrors($validationErrors, 'firstname')?>
+			</div>
+			<div class="<?= getFieldClass($validationErrors, 'lastname')?>">
+				<label for="lastname">Last Name</label>
+				<input type="text" name="lastname" id="lastname"  value="<?= htmlentities($lastName)?>"/>
+				<?= displayErrors($validationErrors, 'lastname')?>
+			</div>
